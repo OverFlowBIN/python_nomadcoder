f8f0ee32a2f285e2673e70f63c8705248c32b0e9
OverFlowBIN <87470206+OverFlowBIN@users.noreply.github.com>
2022-07-11 16:48:00 +0900

    0711 python nomadcoder challenge

---
+++ 1.0 Data Types of Python .py
@@ -0,0 +1,12 @@
+a_string = 'like this'
+a_number = 3
+a_float = 3.12
+a_boolean = False
+a_none = None
+
+print(type(a_string))  # 'str'
+print(type(a_number))  # 'int'
+print(type(a_float))   # 'float'
+print(type(a_boolean)) # 'bool'
+print(type(a_none)) # 'NoneType'
+print(a_none) # 'None'
---
+++ 1.2 Tuples and Dicts.py
@@ -0,0 +1,16 @@
+# Tuple => immutable
+days = ("Mon","Tue","Wed","Thur","Fri")
+print(type(days))  # tuple
+
+
+# dictionary
+alex = {
+  "name" : "Alex",
+  "age" : 29,
+  "Korean" : True,
+  "fav_food" : ["kimchi", "Ssashimi"]  
+}
+
+print(alex['fav_food'])
+alex["hansome"] = True
+print(alex)
